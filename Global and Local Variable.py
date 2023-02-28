71930f8f2d5ad3096a2cf65f3d63fac1c92cae35
gajerabhavik915 <gajerabhavik915@gmail.com>
2023-02-28 18:17:38 -0500

    send

--- Global and Local Variable.py
+++ Global and Local Variable.py
@@ -1,12 +1,12 @@
 print('This programme is for understanding of global and local variable')
 
-a = 10
-def local():
-    a = 15
-    print(a)
-
-local()
-print(a)
+# a = 10
+# def local():
+#     a = 15
+#     print(a)
+#
+# local()
+# print(a)
 
 ##output###
 # 15
@@ -14,13 +14,27 @@ print(a)
 
 #--------------------------------------------------------
 
-a = 20
+# # a = 20
+# def local():
+#     print(a)
+#
+# local()
+# print(a)
+
+###output###
+# 20
+# 20
+
+#---------------------------------------------------------
+
 def local():
+    a = 30
     print(a)
 
 local()
 print(a)
 
 ###output###
-# 20
-# 20
+# 30
+# NameError: name 'a' is not defined
+
