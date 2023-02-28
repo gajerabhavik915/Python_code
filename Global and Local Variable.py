8395c3a2238df071f171ab6b5b69528ba1c964fc
gajerabhavik915 <gajerabhavik915@gmail.com>
2023-02-28 18:33:28 -0500

    send

--- Global and Local Variable.py
+++ Global and Local Variable.py
@@ -1,12 +1,12 @@
 print('This programme is for understanding of global and local variable')
 
-# a = 10
-# def local():
-#     a = 15
-#     print(a)
-#
-# local()
-# print(a)
+a = 10
+def local():
+    a = 15
+    print(a)
+
+local()
+print(a)
 
 ##output###
 # 15
@@ -14,12 +14,12 @@ print('This programme is for understanding of global and local variable')
 
 #--------------------------------------------------------
 
-# # a = 20
-# def local():
-#     print(a)
-#
-# local()
-# print(a)
+# a = 20
+def local():
+    print(a)
+
+local()
+print(a)
 
 ###output###
 # 20
@@ -27,12 +27,12 @@ print('This programme is for understanding of global and local variable')
 
 #---------------------------------------------------------
 
-# def local():
-#     a = 30
-#     print(a)
-#
-# local()
-# print(a)
+def local():
+    a = 30
+    print(a)
+
+local()
+print(a)
 
 ###output###
 # 30
@@ -55,3 +55,29 @@ print(a)
 
 #----------------------------------------------------------------
 
+a = 10
+def local():
+    a = 20
+    global a
+    print(a)
+
+local()
+print(a)
+
+###output###
+# SyntaxError: name 'a' is assigned to before global declaration
+
+#---------------------------------------------------------------------
+
+a = 10
+def local():
+    a = 20
+    globals()['a'] = 30
+    print(a)
+
+local()
+print(a)
+
+###output###
+#20
+#30
