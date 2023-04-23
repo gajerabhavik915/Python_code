7f057ba92b9ae74088f457b1ee139d72ebf8b016
gajerabhavik915 <gajerabhavik915@gmail.com>
2023-04-23 17:09:44 -0400

    Lambda

---
+++ Longest string.py
@@ -0,0 +1,26 @@
+str = ["flower","flow","flight", "florida", "flora", ""]
+
+min_len = len(str[0])
+index1 = 0
+
+
+for index, i in enumerate(str):
+    if len(i) < min_len:
+        min_len = len(i)
+        index1 = index
+
+res = str[index1] + ""
+
+for i in str:
+    for a in range(len(res)):
+        if i[a] == res[a]:
+            continue
+        else:
+            res = res[:a]
+            print(res)
+            break
+
+
+
+# print(min_len)
+# print(index1)
