1969e63011a7fa7ed129bf9e47466ce6143a095b
gajerabhavik915 <gajerabhavik915@gmail.com>
2023-04-22 20:49:11 -0400

    Lambda

--- Roman to Integer.py
+++ Roman to Integer.py
@@ -1,6 +1,6 @@
 import time
 
-a = 'MDXXVIII'
+a = 'MMMMCMLXXXV'
 dict1 = {'I': 1, 'V':5, 'X':10, 'L': 50, 'C':100, 'D' : 500,'M':1000}
 start_time = time.perf_counter()
 len1 = len(a)-1
@@ -8,7 +8,7 @@ val = 0
 
 while len1 > 0:
     if dict1[a[len1]] > dict1[a[len1-1]]:
-        val -= dict1[a[len1-1]] + dict1[a[len1]]
+        val += dict1[a[len1]] - dict1[a[len1-1]]
         len1 = len1 - 2
     elif a[len1]==a[len1-1]:
         val += dict1[a[len1]] + dict1[a[len1-1]]
@@ -18,7 +18,7 @@ while len1 > 0:
         len1 = len1 - 1
 
 if len1 == 0:
-    val += dict1[a[0]]
+    val = val + dict1[a[0]]
 end_time = time.perf_counter()
 
 print(val)
@@ -26,7 +26,7 @@ print(f"Total execution time is {(end_time - start_time)*1e6:.2f} microseconds."
 # -----------------------------------------------------------------
 
 # def romanToInt(s: str) -> int:
-s = 'MDXXVIII'
+s = 'MMMMCMLXXXV'
 dict1 = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
 start_time = time.perf_counter()
 b = len(s) - 1
@@ -43,7 +43,7 @@ print(f"Total execution time is {(end_time - start_time)*1e6:.2f} microseconds."
 #
 # ------------------------------------------------------------------------------------------
 
-s = 'MDXXVIII'
+s = 'MMMMCMLXXXV'
 dict1 = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
 start_time = time.perf_counter()
 i = len(s) - 1    #8
