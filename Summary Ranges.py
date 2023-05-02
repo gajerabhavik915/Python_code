2f3675b9ea8b45975ee9226766ee24fd9d1126e7
gajerabhavik915 <gajerabhavik915@gmail.com>
2023-05-02 13:47:44 -0400

    committing

--- Summary Ranges.py
+++ Summary Ranges.py
@@ -1,26 +1,28 @@
-a1 = [0,2,3,4,6,8,9]
-
+# a1 = [0,2,3,4,6,8,9]
+a1 = [-1,0,5]
 output= []
 
+if len(a1) == 0:
+    print('break')
 
-
-a,b = 0,0
-while b < len(a1) - 1:
-    if a1[b+1] - a1[b] == 1:
-        b += 1
-
-    elif a1[b+1] - a1[b] != 1 and a1[a] == a1[b]:
-        output.append(str(a1[a]))
-        b += 1
-        a = b
-
-    else:
-        output.append(str(a1[a])+'->'+str(a1[b]))
-        b += 1
-        a = b
-if a1[a] == a1[b]:
-    output.append(str(a1[a]))
 else:
-    output.append(str(a1[a]) + '->' + str(a1[b]))
+    a,b = 0,0
+    while b < len(a1) - 1:
+        if a1[b+1] - a1[b] == 1:
+            b += 1
 
-print(output)
+        elif a1[b+1] - a1[b] != 1 and a1[a] == a1[b]:
+            output.append(str(a1[a]))
+            b += 1
+            a = b
+
+        else:
+            output.append(str(a1[a])+'->'+str(a1[b]))
+            b += 1
+            a = b
+    if a1[a] == a1[b]:
+        output.append(str(a1[a]))
+    else:
+        output.append(str(a1[a]) + '->' + str(a1[b]))
+
+    print(output)
