107d285f3c6e26cd5eb16d751638ffc7ace8fa05
gajerabhavik915 <gajerabhavik915@gmail.com>
2023-05-05 19:38:59 -0400

    commit

--- Word Break.py
+++ Word Break.py
@@ -2,5 +2,12 @@
 a = 'leetcode'
 dict = ['lee','le', 'tco', 'de', 'cod']
 
+def wordBreak(s, Dict):
+    for i in Dict:
+        if i[:] == s[:len(i)]:
+            return True
+
+print(wordBreak(a,dict))
+
 
 
