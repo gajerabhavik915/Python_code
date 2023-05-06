d56df0bfa14f4b5a1a37ed688ef88c089c41012b
gajerabhavik915 <gajerabhavik915@gmail.com>
2023-05-06 18:33:27 -0400

    commit

--- Word Break.py
+++ Word Break.py
@@ -1,32 +1,48 @@
 
-a = 'leetcode'
-dict = ['tco','lee','leett', 'de', 'cod']
-# dict = ['lee','leett','tco', 'de', 'cod']
+a = 'leecode'
+# dict = ['tco','leet','leet', 'de', 'cod']
+dict = ['lee','code','leet']
+
 
 
 def wordBreak(a, Dict):
     a1 = a
-    for num in range(len(Dict)):
-        def rerun(a1, Dict):
+    first_ele = a[0]
+    pos = []
+    for loc,i in enumerate(dict):
+        if i[0] == first_ele:
+            pos.append(loc)
+    # print(pos)
 
-            for i in Dict:
-                if len(i) <= len(a1) and i == a1[:len(i)]:
-                    a1 = a1[len(i):]
-                    break
+    for i1 in pos:
+        a1 = a
+        if len(dict[i1]) <= len(a1) and dict[i1] == a1[:len(dict[i1])]:
+            a1 = a1[len(dict[i1]):]
+            # print(a1)
 
-            return a1
+        for num in range(len(Dict)):
+            def rerun(a1, Dict):
 
-        print(a1)
-        a1 = rerun(a1,Dict)
+                for i in Dict:
+                    if len(i) <= len(a1) and i == a1[:len(i)]:
+                        a1 = a1[len(i):]
+                        break
+
+                return a1
+
+            # print(a1)
+            if len(a1) == 0:
+                return False if a1 else True
+            else:
+                a1 = rerun(a1, Dict)
+    return False if a1 else True
+
 
 
 print(wordBreak(a, dict))
 
 
 
-    # if a1:
-    #     srt = rerun(a1, Dict)
-    # elif
 
 
 
