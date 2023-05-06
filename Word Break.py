7fd95ecb335f6e59a0e7ce549364887fa917e8b8
gajerabhavik915 <gajerabhavik915@gmail.com>
2023-05-05 21:22:12 -0400

    commit

--- Word Break.py
+++ Word Break.py
@@ -1,13 +1,38 @@
 
 a = 'leetcode'
-dict = ['lee','le', 'tco', 'de', 'cod']
+dict = ['tco','lee','leett', 'de', 'cod']
+# dict = ['lee','leett','tco', 'de', 'cod']
 
-def wordBreak(s, Dict):
-    for i in Dict:
-        if i[:] == s[:len(i)]:
-            return True
 
-print(wordBreak(a,dict))
+def wordBreak(a, Dict):
+    a1 = a
+    for num in range(len(Dict)):
+        def rerun(a1, Dict):
+
+            for i in Dict:
+                if len(i) <= len(a1) and i == a1[:len(i)]:
+                    a1 = a1[len(i):]
+                    break
+
+            return a1
+
+        print(a1)
+        a1 = rerun(a1,Dict)
+
+
+print(wordBreak(a, dict))
+
+
+
+    # if a1:
+    #     srt = rerun(a1, Dict)
+    # elif
+
+
+
+
+
+
 
 
 
