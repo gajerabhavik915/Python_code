798b516235ce1196154a20b72d52ee24b935b02f
gajerabhavik915 <gajerabhavik915@gmail.com>
2023-05-14 19:33:19 -0400

    Adding code

--- Reverse Linkedlist ll.py
+++ Reverse Linkedlist ll.py
@@ -1,10 +1,10 @@
 # Definition for singly-linked list.
-# class ListNode:
-#     def __init__(self, val=0, next=None):
-#         self.val = val
-#         self.next = next
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
 class Solution:
-    def reverseBetween(self, head: Optional[ListNode], left: int, right: int) -> Optional[ListNode]:
+    def reverseBetween(self, head: [ListNode], left: int, right: int):
         # method 1
         dummy = ListNode()
         dummy.next = head
