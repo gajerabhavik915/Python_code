d5037dac9a5b4c9a226cff4fba2ee683962359b9
gajerabhavik915 <gajerabhavik915@gmail.com>
2023-05-28 21:34:26 -0400

    adding code

--- Iterative Method to find Height of Binary Tree.py
+++ Iterative Method to find Height of Binary Tree.py
@@ -4,6 +4,10 @@
 #         self.val = val
 #         self.left = left
 #         self.right = right
+
+# method-1 : by returning length of result list
+
+
 class Solution:
     def levelOrder(self, root: Optional[TreeNode]) -> List[List[int]]:
         result = []
@@ -23,3 +27,29 @@ class Solution:
                 result.append(append_list)
         print(len(result))
         return result
+
+
+# method-2 : setting count from initial
+
+class Solution1:
+    def levelOrder(self, root: Optional[TreeNode]) -> int:
+        result = []
+        que = collections.deque()
+        que.append(root)
+        count = 0
+
+        while que:
+            len_que = len(que)
+            append_list = []
+            for i in range(len_que):
+                node = que.popleft()
+                if node:
+                    append_list.append(node.val)
+                    que.append(node.left)
+                    que.append(node.right)
+            if append_list:
+                result.append(append_list)
+                count += 1
+        # print(len(result))
+        return count
+
