d53cc0a776ee3c474ab1503aa79bb9c5db27ca39
gajerabhavik915 <gajerabhavik915@gmail.com>
2023-05-29 21:15:57 -0400

    stack

--- Reverse a Stack.py
+++ Reverse a Stack.py
@@ -5,22 +5,24 @@ from typing import List
 
 
 class Solution:
-    # def reverse(self,st):
-    #     if len(st) == 1:
-    #         return st
-    #     len1 = len(st)
-    #     len2 = len(st)//2
+    # method 1
+    def reverse(self,st):
+        if len(st) == 1:
+            return st
+        len1 = len(st)
+        len2 = len(st)//2
 
-    #     def recursion(st, len1, len2, index):
-    #         if len1 == len2:
-    #             return st
-    #         st[len1-1], st[index] = st[index], st[len1-1]
-    #         st = recursion(st, len1-1, len2, index+1)
-    #         return st
+        def recursion(st, len1, len2, index):
+            if len1 == len2:
+                return st
+            st[len1-1], st[index] = st[index], st[len1-1]
+            st = recursion(st, len1-1, len2, index+1)
+            return st
 
-    #     s = recursion(st, len1, len2, 0)
-    #     return s
+        s = recursion(st, len1, len2, 0)
+        return s
 
+    # method 2
     def reverse(self, st):
         if len(st) == 1:
             return st
