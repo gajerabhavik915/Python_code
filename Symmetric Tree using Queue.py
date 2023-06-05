57c2651588b8f29fea35256318dd2ff39cc17884
gajerabhavik915 <gajerabhavik915@gmail.com>
2023-06-05 15:12:27 -0400

    adding code for Tree with queue

--- Symmetric Tree using Queue.py
+++ Symmetric Tree using Queue.py
@@ -1,11 +1,11 @@
 # Definition for a binary tree node.
-# class TreeNode:
-#     def __init__(self, val=0, left=None, right=None):
-#         self.val = val
-#         self.left = left
-#         self.right = right
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
 class Solution:
-    def isSymmetric(self, root: Optional[TreeNode]) -> bool:
+    def isSymmetric(self, root: [TreeNode]) -> bool:
         def left_res1():
             queue = []
             left_res = []
@@ -64,7 +64,45 @@ class Solution:
         else:
             return False
 
+    def isSymmetric(self, root: Optional[TreeNode]) -> bool:
+        if root.left is None or root.right is None:
+            return False
 
+        Q1 = [root.left]
+        Q2 = [root.right]
+
+        # if root.left.val != root.right.val:
+        #     return False
+
+        while Q1 and Q2:
+            node_left = Q1.pop(0)
+            node_right = Q2.pop(0)
+            if node_left and node_right:
+                print(node_left.left)
+                print(node_right.right)
+
+                if node_left.left is None and node_right.right is None:
+                    continue
+                if node_left.right is None and node_right.left is None:
+                    continue
+
+                if (node_left.left and node_right.right):
+                    if node_left.left.val == node_right.right.val:
+                        print(node_left.left.val)
+                        print(node_right.right.val)
+                        Q1.append(node_left.left)
+                        Q2.append(node_right.right)
+                if (node_left.right and node_right.left):
+                    if node_left.right.val == node_right.left.val:
+                        print(node_left.right.val)
+                        print(node_right.left.val)
+                        Q1.append(node_left.right)
+                        Q2.append(node_right.left)
+
+                else:
+                    return False
+
+        return True
 
 
 
