0ebecc3892a8500c94efdfb2875a538460e0388d
gajerabhavik915 <gajerabhavik915@gmail.com>
2023-06-09 21:02:36 -0400

    array coding

---
+++ Majority Element.py
@@ -0,0 +1,19 @@
+class Solution:
+    def majorityElement(self, nums: List[int]) -> int:
+        len1 = len(nums) / 2
+        dict1 = {}  # 3:1, 2:1
+        count = 1
+        for i in nums:
+            if i in dict1:
+                dict1[i] = dict1[i] + 1
+            else:
+                dict1[i] = count
+
+        for element, count in dict1.items():
+            if count > len1:
+                return element
+
+
+
+
+
