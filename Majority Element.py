24115575bb1b955fe3a0b1c2ebd15baba27132d2
gajerabhavik915 <gajerabhavik915@gmail.com>
2023-06-10 18:03:10 -0400

    algorithm

--- Majority Element.py
+++ Majority Element.py
@@ -1,3 +1,22 @@
+# method - 1
+class Solution:
+    def majorityElement(self, nums: List[int]) -> int:
+        count = 0
+        res = 0
+
+        for i in nums:
+            if count == 0:
+                res = i
+            if i != res:
+                count -= 1
+            else:
+                count += 1
+
+        return res
+
+
+# method - 2
+
 class Solution:
     def majorityElement(self, nums: [int]) -> int:
         len1 = len(nums) / 2
@@ -12,8 +31,3 @@ class Solution:
         for element, count in dict1.items():
             if count > len1:
                 return element
-
-
-
-
-
