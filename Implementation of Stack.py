d88da4fb965a697b7f1f7ceee3e41d23c1a5767f
gajerabhavik915 <gajerabhavik915@gmail.com>
2023-06-12 14:09:56 -0400

    stack code

--- Implementation of Stack.py
+++ Implementation of Stack.py
@@ -1,5 +1,6 @@
+
 class Stack:
-    def __init__(self, stack):
+    def __init__(self):
         self.stack = []
         self.len = 0
 
@@ -7,3 +8,25 @@ class Stack:
         if self.len == 0:
             return False
         else:
+            return (self.stack)
+
+    def pop(self):
+        if self.len == 0:
+            return False
+        else:
+            a = self.stack.pop()
+            return a
+
+    def add_element(self, int):
+        self.stack.append(int)
+        self.len += 1
+        return True
+
+
+stack1 = Stack()
+print(stack1.add_element(5))
+stack1.add_element(2)
+print(stack1.print())
+print(stack1.pop())
+print(stack1.print())
+
