5b2ebc644deb6448e58c8d9ae6cc07816e5da01c
gajerabhavik915 <gajerabhavik915@gmail.com>
2023-06-15 22:44:19 -0400

    Algorithm

--- Quick Sort.py
+++ Quick Sort.py
@@ -1,25 +1,34 @@
 
-l = [4,6,9,1,2,13,5,7]
+# l = [4,6,9,1,2,13,5,7]
 
+l = [4,6,5,1,2]
 length = len(l)
-pivot = l[length - 1]
-left = 0
+# # pivot = l[length - 1]
+# left = 0
 right = length - 2
 
-def Quick_sort(l, pivot, left, right):
-    while left < right:
+def Quick_sort(l, left, right):
+    pivot = right+1
+    while left <= right:
         if l[left] <= l[pivot]:
             left += 1
         elif l[right] >= l[pivot]:
             right -= 1
         else:
-            l[left], l[right] =  l[right], l[left]
+            l[left], l[right] = l[right], l[left]
     else:
         l[left], l[pivot] = l[pivot], l[left]
-        print(l)
-        return True
+        return right
 
-# list = Quick_sort(l,pivot,left, right)
+
+def Quick1(l,left, right):
+    if left < right:
+        p = Quick_sort(l,0, len(l) - 2)
+        Quick_sort(l,left,p-1)
+        Quick_sort(l, p+1, len(l) - 2)
+
+list = Quick1(l,0,right)
+print(list)
 
 
 
